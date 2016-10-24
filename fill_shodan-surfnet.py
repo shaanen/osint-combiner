6454aa23cffd22b892d6be171937b870aa40aac1
Sjors Haanen <zjors_93@hotmail.com>
2016-10-24 15:27:22 +0200

    convert str to bytes for sockets

--- fill_shodan-surfnet.py
+++ fill_shodan-surfnet.py
@@ -28,8 +28,7 @@ except socket.error as msg:
 
 try:
     for banner in api.search_cursor("asn:AS1101"):
-        msg = json.dumps(banner)
-        print('msg: ' + msg)
+        msg = json.dumps(banner).encode('utf-8')
         sock.send(msg)
 except shodan.APIError as e:
         print('Error: ', e)
