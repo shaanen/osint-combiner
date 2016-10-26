972d7452cfdd4ac62da9a66a25a4a057385d85cb
Sjors Haanen <zjors_93@hotmail.com>
2016-10-26 14:17:43 +0200

    Some cleanup

--- fill_shodan-surfnet.py
+++ fill_shodan-surfnet.py
@@ -13,26 +13,21 @@ config.read("keys.ini")
 SHODAN_API_KEY = (config['SectionOne']['shodanapikey'])
 
 api = shodan.Shodan(SHODAN_API_KEY)
-
-try:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error as msg:
-    sys.stderr.write("[ERROR] %s\n" % msg[1])
-    sys.exit(1)
-
-try:
-    sock.connect((HOST, PORT))
-except socket.error as msg:
-    sys.stderr.write("[ERROR] %s\n" % msg[1])
-    sys.exit(2)
-
 try:
     for banner in api.search_cursor("asn:AS1101"):
         msg = json.dumps(banner).encode('utf-8')
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.error as msg:
+            sys.stderr.write("[ERROR] %s\n" % msg[1])
+            sys.exit(1)
+        try:
+            sock.connect((HOST, PORT))
+        except socket.error as msg:
+            sys.stderr.write("[ERROR] %s\n" % msg[1])
+            sys.exit(2)
         sock.send(msg)
 except shodan.APIError as e:
         print('Error: ', e)
-
-
 sock.close()
 sys.exit(0)
