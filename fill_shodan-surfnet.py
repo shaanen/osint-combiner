22220ffeae67195fee6aacdd42771a8401fcfe93
Sjors Haanen <zjors_93@hotmail.com>
2016-10-27 10:31:36 +0200

    Added interactive query options: option 1 won't use up query credits.

--- fill_shodan-surfnet.py
+++ fill_shodan-surfnet.py
@@ -6,15 +6,19 @@ import json
 
 HOST = 'localhost'
 PORT = 5040
-
 config = configparser.ConfigParser()
 config.read("keys.ini")
-
 SHODAN_API_KEY = (config['SectionOne']['shodanapikey'])
-
 api = shodan.Shodan(SHODAN_API_KEY)
+
+items = {'1': 'blablablabla', '2': 'asn:AS1101'}
+choice = '0'
+while choice not in items:
+    choice = input("Choose query: (1='blablablabla' 2='asn:AS1101')")
+chosenQuery = items[choice]
+
 try:
-    for banner in api.search_cursor("asn:AS1101"):
+    for banner in api.search_cursor(chosenQuery):
         msg = json.dumps(banner).encode('utf-8')
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- test-shodan.py
+++ test-shodan.py
@@ -7,16 +7,20 @@ import json
 
 HOST = 'localhost'
 PORT = 5040
-
 config = configparser.ConfigParser()
 config.read("keys.ini")
-
 SHODAN_API_KEY = (config['SectionOne']['shodanapikey'])
-
 api = shodan.Shodan(SHODAN_API_KEY)
+
+items = {'1': 'blablablabla', '2': 'asn:AS1101'}
+choice = '0'
+while choice not in items:
+    choice = input("Choose query: (1='blablablabla' 2='asn:AS1101')")
+chosenQuery = items[choice]
+
 try:
     print("going in for loop")
-    for banner in api.search_cursor("apache"):
+    for banner in api.search_cursor(chosenQuery):
         msg = json.dumps(banner).encode('utf-8')
         print("banner: " + json.dumps(banner))
     print("end for loop")
