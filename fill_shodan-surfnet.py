eb46723e2f893f3b0721c93346e0275102595ae8
Sjors Haanen <zjors_93@hotmail.com>
2016-10-28 13:24:25 +0200

    Added custom query

--- fill_shodan-surfnet.py
+++ fill_shodan-surfnet.py
@@ -11,11 +11,13 @@ config.read("keys.ini")
 SHODAN_API_KEY = (config['SectionOne']['SHODAN_API_KEY'])
 api = shodan.Shodan(SHODAN_API_KEY)
 
-items = {'1': 'blablablabla', '2': 'asn:AS1101'}
+items = {'1': 'blablablabla', '2': 'asn:AS1101', '3': 'custom query'}
 choice = '0'
 while choice not in items:
-    choice = input("Choose query: (1='blablablabla' 2='asn:AS1101')")
+    choice = input("Choose query: (1='blablablabla' 2='asn:AS1101' 3='custom query')")
 chosenQuery = items[choice]
+if chosenQuery is items['3']:
+    chosenQuery = input("Enter Query: ")
 
 try:
     for banner in api.search_cursor(chosenQuery):
