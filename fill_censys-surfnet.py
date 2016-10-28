27af1a51546fbb417218640b040045d1fa59a30c
Sjors Haanen <zjors_93@hotmail.com>
2016-10-28 13:42:55 +0200

    Added query choices for censys script

--- fill_censys-surfnet.py
+++ fill_censys-surfnet.py
@@ -11,8 +11,16 @@ config.read("keys.ini")
 CENSYS_API_ID = (config['SectionOne']['CENSYS_API_ID'])
 CENSYS_API_KEY = (config['SectionOne']['CENSYS_API_KEY'])
 
+items = {'2': 'autonomous_system.asn: 1101', '3': 'custom query'}
+choice = '0'
+while choice not in items:
+    choice = input("Choose query: (2='autonomous_system.asn: 1101' 3='custom query')")
+chosenQuery = items[choice]
+if chosenQuery is items['3']:
+    chosenQuery = input("Enter Query: ")
+
 censys = censys.ipv4.CensysIPv4(api_id=CENSYS_API_ID, api_secret=CENSYS_API_KEY)
-for record in censys.search("autonomous_system.asn: 1101"):
+for record in censys.search(chosenQuery):
     msg = json.dumps(record).encode('utf-8')
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
