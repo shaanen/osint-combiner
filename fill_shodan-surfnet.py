2509abd5c5b0068a51f475e170fd9316231defab
Sjors Haanen <zjors_93@hotmail.com>
2016-11-10 12:49:52 +0100

    Another error fix

--- fill_shodan-surfnet.py
+++ fill_shodan-surfnet.py
@@ -18,23 +18,20 @@ while choice not in items:
 chosenQuery = items[choice]
 if chosenQuery is items['3']:
     chosenQuery = input("Enter Query: ")
-
-try:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error as msg:
-    sys.stderr.write("[ERROR] %s\n" % msg[1])
-    sys.exit(1)
-
 try:
     for banner in api.search_cursor(chosenQuery):
         msg = json.dumps(banner).encode('utf-8')
         try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.error as msg:
+            print(msg)
+            sys.exit(1)
+        try:
             sock.connect((HOST, PORT))
         except socket.error as msg:
-            sys.stderr.write("[ERROR] %s\n" % msg[1])
+            print(msg)
             sys.exit(2)
         sock.send(msg)
 except shodan.APIError as e:
         print('Error: ', e)
-sock.close()
 sys.exit(0)
