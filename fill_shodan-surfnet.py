23bf98bd04cbc11d07940d079a5d7b8cfcc5341f
Sjors Haanen <zjors_93@hotmail.com>
2016-11-10 11:09:34 +0100

    fixed socket error in fill_shodan-surfnet.py

--- fill_shodan-surfnet.py
+++ fill_shodan-surfnet.py
@@ -20,13 +20,14 @@ if chosenQuery is items['3']:
     chosenQuery = input("Enter Query: ")
 
 try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error as msg:
+    sys.stderr.write("[ERROR] %s\n" % msg[1])
+    sys.exit(1)
+
+try:
     for banner in api.search_cursor(chosenQuery):
         msg = json.dumps(banner).encode('utf-8')
-        try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        except socket.error as msg:
-            sys.stderr.write("[ERROR] %s\n" % msg[1])
-            sys.exit(1)
         try:
             sock.connect((HOST, PORT))
         except socket.error as msg:
