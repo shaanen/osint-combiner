6df484ccb4ed0f0d0dc9e68f8399a1bfc76d9861
Sjors Haanen <zjors_93@hotmail.com>
2016-11-11 11:03:26 +0100

    added tofile-censys.py

---
+++ tofile-censys.py
@@ -0,0 +1,23 @@
+import censys.ipv4
+import configparser
+import json
+
+config = configparser.ConfigParser()
+config.read("keys.ini")
+CENSYS_API_ID = (config['SectionOne']['CENSYS_API_ID'])
+CENSYS_API_KEY = (config['SectionOne']['CENSYS_API_KEY'])
+
+items = {'2': 'autonomous_system.asn: 1101', '3': 'custom query'}
+choice = '0'
+while choice not in items:
+    choice = input("Choose query: (2='autonomous_system.asn: 1101' 3='custom query')")
+chosenQuery = items[choice]
+if chosenQuery is items['3']:
+    chosenQuery = input("Enter Query: ")
+
+censys = censys.ipv4.CensysIPv4(api_id=CENSYS_API_ID, api_secret=CENSYS_API_KEY)
+
+with open("outputfile-censys.json", "a") as outputfile:
+    for record in censys.search(chosenQuery):
+        outputfile.write(json.dumps(record) + "\n")
+
