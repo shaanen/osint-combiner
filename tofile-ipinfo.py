416839c4b233146822534ca4b336d11a9c3f7a29
Sjors Haanen <zjors_93@hotmail.com>
2016-12-13 14:24:13 +0100

    On the fly progress prints for requests done/failed

--- tofile-ipinfo.py
+++ tofile-ipinfo.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
-# Reads CIDRs from file, uses ipinfo for each IP, saves output to outputfiles/ipinfo/ipinfo.json
+# Reads CIDRs from user input, uses ipinfo for each IP, saves output to outputfiles/ipinfo/ipinfo.json
 
-from netaddr import IPNetwork
 import threading
 import requests
-import logging
 import json
 from datetime import datetime
 import time
@@ -16,6 +14,8 @@ headers = {'Content-Type': 'text/plain', 'Accept': 'text/json'}
 path_output_file = 'outputfiles/ipinfo/ipinfo.json'
 result_list = []
 threads = []
+done_counter = 0
+done_counter_lock = threading.Lock()
 failed_counter = 0
 failed_counter_lock = threading.Lock()
 IPs = base.get_cidr_from_user_input()
@@ -28,19 +28,24 @@ class GetIpInfoThread (threading.Thread):
         self.target_ip = target_ip
 
     def run(self):
+        global done_counter
+        global failed_counter
         got_valid_response = False
         while not got_valid_response:
+            print('\r' + str(done_counter) + ' done, ' + str(failed_counter) + ' failed', end='')
             try:
                 resp = requests.post(url, headers=headers, data=str(self.target_ip))
                 resp_json = json.loads(resp.text)
                 resp_json['timestamp'] = str(datetime.now())
                 result_list.append(json.dumps(resp_json))
                 got_valid_response = True
-            except requests.exceptions.ConnectionError:
-                global failed_counter
+                with done_counter_lock:
+                    done_counter += 1
+            # except requests.exceptions.ConnectionError:
+            except:
                 with failed_counter_lock:
                     failed_counter += 1
-                # logging.warning("ConnectionError for ip %s" % self.target_ip + " retrying now...")
+        print('\r' + str(done_counter) + ' done, ' + str(failed_counter) + ' failed', end='')
 
 print('---Getting info for CIDR ' + str(IPs) + ' (' + str(IPs.size) + ' total)---')
 start_time = time.time()
@@ -54,18 +59,17 @@ for ip in IPs:
 # Wait for all GetIpInfoThreads to complete
 for thread in threads:
     thread.join()
+print('')
 
 # Print useful statistics
-print(str(IPs.size) + ' IPs in CIDR ' + str(IPs))
 print(str(failed_counter) + ' times retried an IP')
 print(str(round((time.time() - start_time))) + ' seconds needed for getting all responses')
 
 # Write all responses to file
-with open(path_output_file, "a") as output_file:
+with open(path_output_file, 'a') as output_file:
     # Writing newline if file is not empty
     if os.stat(path_output_file).st_size != 0:
         output_file.write('\n')
 
     output_file.write('\n'.join(result_list))
-print('\r' + str(len(result_list)) + " results written in " + path_output_file, end='')
-
+print('\r' + str(len(result_list)) + ' results written in ' + path_output_file, end='')
