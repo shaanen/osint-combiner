61fbb9f4b0d96086493eb1fc96492994dc64f433
Sjors <zjors_93@hotmail.com>
2017-01-13 15:36:32 +0100

    Moved test scripts to debug/ and added some scripts for develop purposes

---
+++ debug/analyse-json-file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import json
+from pathlib import Path
+
+# DEBUG Script for printing and counting presence of specific JSON fields from given file
+
+total_count = 0
+present_count = 0
+input_file = Path('../outputfiles/ipinfo/ipinfo-mongodb-converted.json')
+for element in input_file.open():
+    total_count += 1
+    json_element = json.loads(element)
+    try:
+        # print JSON element(s), if present
+        print(str(json_element['ip']) + ' | ' + str(type(json_element['ipinfo.whois']['person']['last_modified'])))
+        present_count += 1
+    except KeyError:
+        pass
+# print statistics
+print('Given element(s) present in: ' + str(present_count) + '/' + str(total_count) + 'total')
+print('Given element(s) missing: ' + str(total_count - present_count))
