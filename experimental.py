02d95a17404afe7c26d931a252e1008adc200db6
Sjors <zjors_93@hotmail.com>
2017-01-21 19:41:45 +0100

    Removed the now old debug directory

--- experimental.py
+++ experimental.py
@@ -1,1 +1,30 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+import json
+from pathlib import Path
+
+# DEBUG Script for printing and counting presence of specific JSON fields from given file
+list_of_protocols = []
+total_count = 0
+present_count = 0
+input_file = Path('inputfiles/mapping.json')
+for element in input_file.open():
+    total_count += 1
+    json_element = json.loads(element)
+    for key in json_element['properties']:
+        print (str(key) + ' | ' + str(len(str(json_element['properties'][key]))))
+        #protocol = json_element['censys'][str_protocol]
+        #print(str_protocol + ' | ' + str(len(str(protocol))))
+        #list_of_protocols.append()
+        #list_of_protocols[str(len(str(protocol)))] = str_protocol
+#print(str(sorted(list_of_protocols)))
+    # print(type(json_element['properties']))
+    # try:
+    #     # print JSON element(s), if present
+    #     print(str(json_element['ip']))
+    #     present_count += 1
+    # except KeyError:
+    #     pass
+# print statistics
+print('-----------')
+print('Given element(s) present in: ' + str(present_count) + '/' + str(total_count) + 'total')
+print('Given element(s) missing: ' + str(total_count - present_count))
--- temp.py
+++ temp.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python
+import re
+input_dict = {'p25': 'blabla25', 'p80': 'blabla80', 'p110': 'blabla110', 'p995': 'blabla995'}
+#  Remove 'p' from every protocol key
+pattern = re.compile("^p[0-9]{1,6}$")
+for key in input_dict:
+    if pattern.match(key):
+        input_dict[key[1:]] = input_dict[key]
+        del input_dict[key]
+print(str(input_dict))
