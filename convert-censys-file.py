c862ee0995219328f91a45bce0c873add9c9469b
Sjors <zjors_93@hotmail.com>
2017-01-21 15:13:53 +0100

    Added check for empty lines in converters

--- convert-censys-file.py
+++ convert-censys-file.py
@@ -20,9 +20,10 @@ if choice is 'f':
                                                     + '-converted' + os.path.splitext(str(input_file))[1])
     with open(str_path_output_file, 'a', encoding='utf-8') as output_file:
         for str_banner in input_file.open(encoding='utf8'):
-            banner = dict_clean_empty(json.loads(str_banner))
-            censys.to_es_convert(censys, banner)
-            output_file.write(json.dumps(banner) + '\n')
+            if str_banner != '\n':
+                banner = dict_clean_empty(json.loads(str_banner))
+                censys.to_es_convert(censys, banner)
+                output_file.write(json.dumps(banner) + '\n')
     print('Converted ' + str(input_file.as_posix()) + ' to ' + str_path_output_file)
 elif choice is 'd':
     input_directory = ask_input_directory()
@@ -41,7 +42,8 @@ elif choice is 'd':
         print('\r' + 'Converting ' + input_file + '[' + str(counter) + '/' + str(len(files_to_convert)) + ']...', end='')
         with open(str_output_file, 'a') as output_file:
             for str_banner in open(input_directory + '/' + input_file, 'r'):
-                banner = dict_clean_empty(json.loads(str_banner))
-                censys.to_es_convert(censys, banner)
-                output_file.write(json.dumps(banner) + '\n')
+                if str_banner != '\n':
+                    banner = dict_clean_empty(json.loads(str_banner))
+                    censys.to_es_convert(censys, banner)
+                    output_file.write(json.dumps(banner) + '\n')
     print('\nConverted files written in ' + output_directory)
--- convert-ipinfo-file.py
+++ convert-ipinfo-file.py
@@ -41,7 +41,8 @@ elif choice is 'd':
         print('\r' + 'Converting ' + input_file + '[' + str(counter) + '/' + str(len(files_to_convert)) + ']...', end='')
         with open(str_output_file, 'a') as output_file:
             for str_banner in open(input_directory + '/' + input_file, 'r'):
-                banner = dict_clean_empty(json.loads(str_banner))
-                ipinfo.to_es_convert(ipinfo, banner)
-                output_file.write(json.dumps(banner) + '\n')
+                if str_banner != '\n':
+                    banner = dict_clean_empty(json.loads(str_banner))
+                    ipinfo.to_es_convert(ipinfo, banner)
+                    output_file.write(json.dumps(banner) + '\n')
     print('\nConverted files written in ' + output_directory)
